53e963d31be69392aa608dbe9ff64e145993ed79
enaganti204 <srilatha.enaganti204@gmail.com>
2024-05-10 05:57:10 +0530

    hello world

---
+++ src/main/java/com/spring/hello/SpringBootHelloWorldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldExampleApplication
+{
+public static void main(String[] args) 
+{
+SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+}
+}
